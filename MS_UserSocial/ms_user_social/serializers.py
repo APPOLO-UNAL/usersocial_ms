39e0bea2e21f14b2cae34e8f8fa4af8d54c84cdf
Lanna-27 <lalacast2000@gmail.com>
2024-03-08 09:59:08 -0500

    correction serializers, update readme

--- MS_UserSocial/ms_user_social/serializers.py
+++ MS_UserSocial/ms_user_social/serializers.py
@@ -1,23 +1,27 @@
 from rest_framework import serializers
-from models import *
+from models import User
 
 class ProductSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-        user = User(name = validated_data.get("name"),
-                          description = validated_data.get("description"),
-                          quantity = validated_data.get("quantity"))
+        newUser = User(emailAddr = validated_data.get("email"),
+                          userName = validated_data.get("userName"),
+                          keyIdAuth = validated_data.get("keyIdAuth"),
+                          idArtist = validated_data.get("idArtist"),
+                          idTracks = validated_data.get("idTracks"),
+                          idAlbums = validated_data.get("idAlbums"))
                           
-        user.save()
-        return user
+        newUser.save()
+        return newUser
     
     def update(self, instance, validated_data):
         user = User.nodes.get(instance.username)
         if (user):
-            instance.name = validated_data.get("name")
-            instance.description = validated_data.get("description")
-            instance.quantity = validated_data.get("quantity")
-            instance.category = validated_data.get("category")
-            instance.save()
-            return instance
+            user.emailAddr = validated_data.get("email")
+            user.userName = validated_data.get("name")
+            user.description = validated_data.get("description")
+            user.quantity = validated_data.get("quantity")
+            user.category = validated_data.get("category")
+            user.save()
+            return user
         else:
             return "User not found"
